264faaccb0dc3c9b5d39432072a85252e713713e
Nikita <nikita.silchenko.work@yandex.ru>
2021-11-02 22:24:31 +0300

    Added placeholders to the iFrame and Related videos.

--- assets/js/searchVideo.js
+++ assets/js/searchVideo.js
@@ -1,5 +1,5 @@
 const iFrameVideo = document.getElementById("js-youtube-iframe");
-const API_KEY = "AIzaSyC6OEeV1De1LWqluWcFqhdxByW9UoiiSD0";
+const API_KEY = "AIzaSyDI1PdphjoZjJITEiibMSEk7vk_ZM_3qr8";
 
 const getVideoID = () => {
   var paramsString = document.location.search; // ?page=4&limit=10&sortby=desc
@@ -23,9 +23,11 @@ const getVideoDuration = (duration) => {
 };
 
 const showVideo = () => {
-  $.getJSON(
-    `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${getVideoID()}&key=${API_KEY}`,
-    function (data) {
+  $.ajax({
+    dataType: "json",
+    type: "GET",
+    url: `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${getVideoID()}&key=${API_KEY}`,
+    success: (data) => {
       data = data.items[0];
       let day = new Date(data.snippet.publishedAt).getDate();
       let month = new Date(data.snippet.publishedAt).getMonth() + 1;
@@ -33,68 +35,71 @@ const showVideo = () => {
       let year = new Date(data.snippet.publishedAt).getFullYear();
 
       let htmlData = `
-      <iframe
-        src="https://www.youtube.com/embed/${getVideoID()}?rel=0&amp;modestbranding=1&amp;showinfo=0&amp;controls=1&amp;iv_load_policy=3&amp;autoplay=0&amp;enablejsapi=1" 
-        seamless="" 
-        frameborder="0" 
-        allowfullscreen="" 
-        allow="autoplay; encrypted-media; fullscreen"
-      ></iframe>
-  <div class="video-player__video-info video-info">
-      <div class="video-info__name">${data.snippet.title}</div>
-      <div class="video-info__sub-info">
-          <div class="video__chips">
-              <div class="chip">
-                  <div class="chip__icon">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                          viewBox="0 0 20 20" fill="currentColor">
-                          <path fill-rule="evenodd"
-                              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                              clip-rule="evenodd" />
-                      </svg>
-                  </div>
-                  <div class="chip__text">${getLikesPercent(
-                    data.statistics
-                  )}%</div>
-              </div>
-              <div class="chip">
-                  <div class="chip__icon">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                          viewBox="0 0 20 20" fill="currentColor">
-                          <path fill-rule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                              clip-rule="evenodd" />
-                      </svg>
-                  </div>
-                  <div class="chip__text">${getVideoDuration(
-                    data.contentDetails.duration
-                  )}</div>
-              </div>
-              <div class="chip">
-                  <div class="chip__text">${
-                    data.statistics.viewCount
-                  } Views</div>
-              </div>
-          </div>
-          <div class="video-info__channel-name">${
-            data.snippet.channelTitle
-          }</div>
-          <div class="video-info__video-date">${day}.${month}.${year}</div>
-      </div>
-      <div class="video-info__description">${
-        data.snippet.localized.description
-      }</div>
-  </div>
-      `;
+        <iframe
+          src="https://www.youtube.com/embed/${getVideoID()}?rel=0&amp;modestbranding=1&amp;showinfo=0&amp;controls=1&amp;iv_load_policy=3&amp;autoplay=0&amp;enablejsapi=1" 
+          seamless="" 
+          frameborder="0" 
+          allowfullscreen="" 
+          allow="autoplay; encrypted-media; fullscreen"
+        ></iframe>
+    <div class="video-player__video-info video-info">
+        <div class="video-info__name">${data.snippet.title}</div>
+        <div class="video-info__sub-info">
+            <div class="video__chips">
+                <div class="chip">
+                    <div class="chip__icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="chip__text">${getLikesPercent(
+                      data.statistics
+                    )}%</div>
+                </div>
+                <div class="chip">
+                    <div class="chip__icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="chip__text">${getVideoDuration(
+                      data.contentDetails.duration
+                    )}</div>
+                </div>
+                <div class="chip">
+                    <div class="chip__text">${
+                      data.statistics.viewCount
+                    } Views</div>
+                </div>
+            </div>
+            <div class="video-info__channel-name">${
+              data.snippet.channelTitle
+            }</div>
+            <div class="video-info__video-date">${day}.${month}.${year}</div>
+        </div>
+        <div class="video-info__description">${
+          data.snippet.localized.description
+        }</div>
+    </div>
+        `;
       $(".video-player__iframe").append(htmlData);
-    }
-  );
+      $(".js-loading--iframe").remove();
+    },
+  });
 };
 
 const loadRelatedVideos = () => {
-  $.getJSON(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${getVideoID()}&type=video&key=${API_KEY}`,
-    (data) => {
+  $.ajax({
+    dataType: "json",
+    type: "GET",
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${getVideoID()}&type=video&key=${API_KEY}`,
+    success: (data) => {
       data.items
         .filter((el, index) => index !== data.items.length - 1)
         .map((video, index) => {
@@ -113,8 +118,9 @@ const loadRelatedVideos = () => {
             </a>
           `);
         });
-    }
-  );
+      $(".js-loading--related").remove();
+    },
+  });
 };
 
 $(document).ready(() => {

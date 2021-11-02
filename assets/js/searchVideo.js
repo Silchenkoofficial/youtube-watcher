73263f1dd7f24f71362cf1351e18b3adb6c5cace
Nikita <nikita.silchenko.work@yandex.ru>
2021-11-02 22:49:43 +0300

    Removed jQuery

--- assets/js/searchVideo.js
+++ assets/js/searchVideo.js
@@ -22,12 +22,12 @@ const getVideoDuration = (duration) => {
     .replace("PT", "");
 };
 
-const showVideo = () => {
-  $.ajax({
-    dataType: "json",
-    type: "GET",
-    url: `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${getVideoID()}&key=${API_KEY}`,
-    success: (data) => {
+const showVideo = async () => {
+  await fetch(
+    `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${getVideoID()}&key=${API_KEY}`
+  )
+    .then((data) => data.json())
+    .then((data) => {
       data = data.items[0];
       let day = new Date(data.snippet.publishedAt).getDate();
       let month = new Date(data.snippet.publishedAt).getMonth() + 1;
@@ -39,7 +39,7 @@ const showVideo = () => {
           src="https://www.youtube.com/embed/${getVideoID()}?rel=0&amp;modestbranding=1&amp;showinfo=0&amp;controls=1&amp;iv_load_policy=3&amp;autoplay=0&amp;enablejsapi=1" 
           seamless="" 
           frameborder="0" 
-          allowfullscreen="" 
+          allowfullscreen 
           allow="autoplay; encrypted-media; fullscreen"
         ></iframe>
     <div class="video-player__video-info video-info">
@@ -88,97 +88,53 @@ const showVideo = () => {
         }</div>
     </div>
         `;
-      $(".video-player__iframe").append(htmlData);
-      $(".js-loading--iframe").remove();
-    },
-  });
+
+      document.querySelector(".video-player__iframe").innerHTML = htmlData;
+      document
+        .querySelectorAll(".js-loading--iframe")
+        .forEach((e) => e.remove());
+    });
 };
 
-const loadRelatedVideos = () => {
-  $.ajax({
-    dataType: "json",
-    type: "GET",
-    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${getVideoID()}&type=video&key=${API_KEY}`,
-    success: (data) => {
+const loadRelatedVideos = async () => {
+  await fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${getVideoID()}&type=video&key=${API_KEY}`
+  )
+    .then((data) => data.json())
+    .then((data) => {
       data.items
-        .filter((el, index) => index !== data.items.length - 1)
-        .map((video, index) => {
-          video.id &&
-            video.snippet.thumbnails &&
-            video.snippet.title &&
-            $("#js-related-videos").append(`
-          <a href="/v/?id=${video?.id?.videoId}" class="video">
-            <div class="video__image">
-                <img
-                src="${video?.snippet?.thumbnails?.high?.url}"
-                alt="Related image"
-                />
-            </div>
-            <div class="video__title">${video?.snippet?.title}</div>
-            </a>
-          `);
+        .filter((_, index) => index !== data.items.length - 1)
+        .map((video) => {
+          if (video.id && video.snippet.thumbnails && video.snippet.title) {
+            let videoLink = document.createElement("a");
+            videoLink.classList.add("video");
+            videoLink.href = `/v/?id=${video?.id?.videoId}`;
+
+            let videoImage = document.createElement("div");
+            videoImage.classList.add("video__image");
+            videoImage.innerHTML = `<img src="${video?.snippet?.thumbnails?.high?.url}" alt="Related image" />`;
+
+            let videoTitle = document.createElement("div");
+            videoTitle.classList.add("video__title");
+            videoTitle.textContent = video?.snippet?.title;
+
+            videoLink.appendChild(videoImage);
+            videoLink.appendChild(videoTitle);
+
+            document.querySelector("#js-related-videos").appendChild(videoLink);
+          }
         });
-      $(".js-loading--related").remove();
-    },
-  });
+      document
+        .querySelectorAll(".js-loading--related")
+        .forEach((e) => e.remove());
+    });
 };
 
-$(document).ready(() => {
-  $(".video-page").val(localStorage.getItem("video-url"));
+document.addEventListener("DOMContentLoaded", function () {
   if (iFrameVideo) {
+    document.querySelector(".video-page").value =
+      localStorage.getItem("video-url");
     showVideo();
     loadRelatedVideos();
   }
 });
-
-{
-  /* <a href="/v/?id=${video.id.videoId}" class="video">
-  <div class="video__image">
-    <img src="${video.snippet.thumbnails.standard.url}" alt="Related image" />
-  </div>
-  <div class="video__title">${video.snippet.title}</div>
-  <div class="video__chips">
-    <div class="chip">
-      <div class="chip__icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-      <div class="chip__text">${getLikesPercent(video.statistics)}%</div>
-    </div>
-    <div class="chip">
-      <div class="chip__icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-      <div class="chip__text">
-        ${getVideoDuration(video.contentDetails.duration)}
-      </div>
-    </div>
-    <div class="chip">
-      <div class="chip__text">1172 Views</div>
-    </div>
-  </div>
-</a>; */
-}

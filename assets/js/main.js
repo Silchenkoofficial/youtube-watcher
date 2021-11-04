da8f53b5bd396d6d7f244f8d2a590c988fd715b4
Nikita <nikita.silchenko.work@yandex.ru>
2021-11-04 13:12:39 +0300

    Fixed some mistakes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@ let searchForm = document.getElementById("js-search-form");
 let searchInput = document.getElementById("js-search-input");
 
 // Function of adding current year;
-copyrightYear.innerText = new Date().getFullYear();
+copyrightYear.textContent = new Date().getFullYear();
 
-// Actions when you click the "Search" button
+// // Actions when you click the "Search" button
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   localStorage.setItem("video-url", searchInput.value);
   const videoID = [...searchInput.value.split("v=")][1];
-  location.href = `/v/?id=${videoID}`;
+  window.location = `${document.location.protocol}//${document.location.host}/v/?id=${videoID}`;
 });
